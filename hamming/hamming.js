0d017e60b05c6ad6fe63934644a5891e2d96f2ff
Jen Trudell <jtrudell@gmail.com>
2016-02-01 20:05:18 -0600

    refactor to make it shorter, still not please with iteration of second string

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,21 +1,18 @@
-let countMatches = (stringOne, stringTwo) => {
-  let i = 0;
-  let total = 0;
-  for (var letter of stringOne) {
-    if (letter !== stringTwo[i]) {
-      total++;
-    }
-    i++;
-  }
-  return total;
-};
-
 class Hamming {
   compute(stringOne, stringTwo) {
     if (stringOne.length !== stringTwo.length) {
       throw new Error('DNA strands must be of equal length.');
     }
-    return countMatches(stringOne, stringTwo);
+    
+    let i = 0;
+    let total = 0;
+    for (let letter of stringOne) {
+      if (letter !== stringTwo[i]) {
+        total++;
+      }
+      i++;
+    }
+    return total;
   }
 }
 
