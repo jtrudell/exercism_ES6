796f4aef4b888dc5c3faa989d4bd0e1f135934df
Jen <jtrudell@gmail.com>
2017-10-28 08:13:20 -0500

    pangram solution

---
+++ pangram/pangram.js
@@ -0,0 +1,21 @@
+class Pangram {
+  constructor(str) {
+    this.str = str.toLowerCase();
+  }
+
+  isPangram() {
+    if (this.str.length < 26) {
+      return false
+    };
+
+    for (let cp = 97; cp <= 122; cp++) {
+      if (this.str.indexOf(String.fromCharCode(cp)) === -1) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+export default Pangram;
